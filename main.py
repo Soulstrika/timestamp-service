b53b2cb6250091daa2d4380645a16181ab8e6d6e
Soulstrika <soulstriker@gmail.com>
2026-04-25 00:41:45 -0300

    Update main.py
    self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
    self.send_header('Pragma', 'no-cache')
    self.send_header('Expires', '0')

--- main.py
+++ main.py
@@ -42,6 +42,9 @@ class TimestampHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.send_header('Content-Length', len(body))
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
         self.end_headers()
         self.wfile.write(body)
 
